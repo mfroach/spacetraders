a9e65765e5c7dd6da8349ae6821902637780aac2
mfro <mfro@iridium-mbp.local>
2023-08-21 20:15:49 -0500

    update

--- console.rb
+++ console.rb
@@ -18,10 +18,13 @@ module TradeConsole
         return response
     end
     def self.handle_cmds(input) #handle user input
-        @input = input
+        @input = input.split
         @cmds = ['help', 'balance', 'location', 'contracts', 'quit']
-        if @cmds.include? @input
-            self.public_send(@input)
+        if @cmds.include? @input[0]
+        and input[1].nil?
+            public_send(@input[0])
+        elsif not input[1].nil?
+            puts "#{@input[0]}.""#{@input[1]}"
         else
             puts "Bad input."
         end
@@ -46,14 +49,22 @@ module TradeConsole
         puts "A galaxy far, far away"
     end
 
-    def self.contracts # check for available contracts
+    def self.contracts(arg1) # check for available contracts
         sub_uri = '/my/contracts'
         responseHash = JSON.parse(request(sub_uri).body) # todo: make an actual parser
-        puts responseHash.dig("data", 0, "id")
+        puts responseHash.fetch("data")[0].fetch("id")
+        # responseHash.each { |key, value| p "#{key}: #{value}" }
+
     end
 
     def self.quit
         abort "quitting"
+    end
+end
+
+class Contracts
+    def self.accept()
+        puts "Accepted!"
     end
 end
 
